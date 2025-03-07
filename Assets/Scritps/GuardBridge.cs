2bafc2847fde997a6b2fe3084d72a17483d35624
EduardoTessari <tessari702@gmail.com>
2025-03-07 20:52:07 -0300

    1. Adicionado a entrega do item, 2. Adicionado o Conserto da Ponte, 3. Adicionado a reduçao do inventário quanto entrega o item

--- Assets/Scritps/GuardBridge.cs
+++ Assets/Scritps/GuardBridge.cs
@@ -1,14 +1,25 @@
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GuardBridge : MonoBehaviour
 {
     [SerializeField]private Canvas _canvas;
+    [SerializeField]private int _stonesToRepair;
+    private bool _isOpen;
+
+    [SerializeField] private GameObject _closeBridge;
+    private InventoryManager _inventoryManager;
+    
+
 
 
     private void Awake()
     {
+        _isOpen = false;
         _canvas.gameObject.SetActive(false);
+        _inventoryManager = FindAnyObjectByType<InventoryManager>();
 
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -25,6 +36,31 @@ public class GuardBridge : MonoBehaviour
 
 
         }
+    }
+
+    public void OpenBridge()
+    {
+        if (!_isOpen)
+        {
+            if (_inventoryManager.stone >= _stonesToRepair)
+            {
+                _closeBridge.SetActive(false);
+                _isOpen = true;
+                _canvas.gameObject.SetActive(false);
+                Debug.Log("Siga seu caminho");
+                _inventoryManager.stone = _inventoryManager.stone - _stonesToRepair;
+                Debug.Log(_inventoryManager.stone);
+            }
+            else
+            {
+                Debug.Log("Colete mais itens");
+            }
+        }
+        else
+        {
+            Debug.Log("O caminho ja está aberto");
+        }
+        
     }
 
     public void CloseCanvas()
